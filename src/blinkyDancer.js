cc675dc0a247e1215313792c1299ab57d2bc4b34
alexvision <alex.manusu@gmail.com>
2015-08-27 15:37:21 -0700

    Blinky now has colours and shapes

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -9,6 +9,24 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.oldStep = BlinkyDancer.prototype.step;
 
 BlinkyDancer.prototype.step = function() {
+  var colours = ["red", "blue", "yellow", "green"];
+  var colour = colours[Math.floor(Math.random()*colours.length)];
+
+  var shapes = ["circle", "square", "triangle"];
+  var shape = shapes[Math.floor(Math.random()*shapes.length)];
+
   this.oldStep();
   this.$node.toggle();
+
+  if (this.$node.is(':visible')) {
+    colours.forEach(function(colour) {
+      this.$node.removeClass("blinky-"+colour);
+    }, this);
+    this.$node.addClass("blinky-"+colour);
+
+    shapes.forEach(function(shape){
+      this.$node.removeClass("blinky-"+shape);
+    },this);
+    this.$node.addClass("blinky-"+shape);
+  }
 };
